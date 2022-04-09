6266c7758edced3efb6f4a1e177ace0c94ccfe85
Jerome Tokumei <jerometokumei@gmail.com>
2022-04-09 22:01:02 +0800

    Responsive repo preview

---
+++ index.js
@@ -0,0 +1,50 @@
+import reposData from './repos.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const repos = document.querySelector('.repos');
+  reposData.forEach(data => {
+    repos.appendChild(CreateCard(data));
+  });
+})
+
+function CreateCard(data) {
+  const project = document.createElement('article');
+  project.classList.add('project');
+
+  const title = document.createElement('h2');
+  title.innerText = data.title;
+  project.appendChild(title);
+
+  const image = document.createElement('img');
+  image.src = data.image;
+  project.appendChild(image);
+
+  const techUsed = document.createElement('div');
+  techUsed.classList.add('tech');
+  techUsed.innerText = 'Tech used: '
+  project.appendChild(techUsed);
+
+  const list = document.createElement('ul');
+  data.technologies.forEach(tech => {
+    list.appendChild(CreateLi(tech));
+  })
+  project.appendChild(list);
+
+  const description = document.createElement('p');
+  description.classList.add('description');
+  description.innerText = data.description;
+  project.appendChild(description);
+
+  const link = document.createElement('a');
+  link.classList.add('repo-link');
+  link.href = data.path;
+  link.appendChild(project);
+
+  return link;
+}
+
+function CreateLi(input) {
+  const li = document.createElement('li');
+  li.innerText = input;
+  return li;
+}
--- repos.js
+++ repos.js
@@ -1,13 +1,25 @@
-
+// change it to json file
 const repos = [
   {
-    "title": "Interactive-Rating",
-    "path": "/interactive-rating-component-main/index.html",
-    "image": "#",
-    "gif": "#",
-    "live": "#",
-    "technologies": "",
-    "description": ""
+    title: 'Interactive-Rating',
+    path: './interactive-rating-component-main/index.html',
+    image: './interactive-rating-component-main/design/mobile-design.jpg',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    description: 'This is a nice, small project to practice handling user interactions and updating the DOM. Perfect for anyone who has learned the basics of JavaScript!'
+  },
+  {
+    title: 'Interactive-Rating',
+    path: './interactive-rating-component-main/index.html',
+    image: './interactive-rating-component-main/design/mobile-design.jpg',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    description: 'This is a nice, small project to practice handling user interactions and updating the DOM. Perfect for anyone who has learned the basics of JavaScript!'
+  },
+  {
+    title: 'Interactive-Rating',
+    path: './interactive-rating-component-main/index.html',
+    image: './interactive-rating-component-main/design/mobile-design.jpg',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    description: 'This is a nice, small project to practice handling user interactions and updating the DOM. Perfect for anyone who has learned the basics of JavaScript!'
   }
 ]
 
