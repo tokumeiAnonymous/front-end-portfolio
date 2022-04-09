466a96f936e0136ba42ea3087dcd34c5c24f775d
Jerome Tokumei <jerometokumei@gmail.com>
2022-04-09 17:46:30 +0800

    Changed json file into js file

---
+++ repos.js
@@ -0,0 +1,14 @@
+
+const repos = [
+  {
+    "title": "Interactive-Rating",
+    "path": "/interactive-rating-component-main/index.html",
+    "image": "#",
+    "gif": "#",
+    "live": "#",
+    "technologies": "",
+    "description": ""
+  }
+]
+
+export default repos;
